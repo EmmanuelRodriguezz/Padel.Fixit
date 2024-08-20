53377e0fd5fe383d665ad3769700e703ffb24387
Emmanuel Rodriguez <emmacasbas1608@gmail.com>
2024-08-19 21:31:49 -0300

    m5u4

--- back/routes/admin/login.js
+++ back/routes/admin/login.js
@@ -1,10 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
 
 router.get('/', function (req, res, next) {
-    res.render('admin/login' , {
+    res.render('admin/login', {
         layout: 'admin/layout'
     })
 });
 
+router.get('/logout',function (req, res, next) {
+    req.session.destroy();
+    res.render('admin/login', {
+        layout: 'admin/layout'
+    });
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        var usuario = req.body.usuario;
+        var password = req.body.password;
+
+        var data = await usuariosModel.getUserByUsernameAndPassword
+            (usuario, password);
+
+
+        if (data != undefined) {
+
+            req.session.id_usuario = data.usuario;
+            req.session.nombre = data.usuario;
+
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/login', {
+                layout: 'admin/layout',
+                error: true
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
 module.exports = router;
+
