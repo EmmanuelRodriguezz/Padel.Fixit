06ef1d21a6ad226e8fa4a64361b4c4bbcae9a8a5
Emmanuel Rodriguez <emmacasbas1608@gmail.com>
2024-08-28 23:32:27 -0300

    TRABAJO FINAL

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,7 +1,35 @@
 const Footer = (props) => {
     return (
         <footer>
-            <p>derechos reservados. Emmanuel Rodriguez</p>
+            <footer>
+                    <div class="footer-container">
+                        <section class="footer-section">
+                            <h5>Padel.Fixit</h5>
+                            <ul>
+                                <li><i class="fa fa-map-marker"></i> Argerich 2557</li>
+                                <li><i class="fa fa-phone"></i> 11 3692-2820</li>
+                                <li><i class="fa fa-envelope"></i> info@company.com</li>
+                            </ul>
+                        </section>
+                        <section class="footer-section">
+                            <h5>Further Info</h5>
+                            <ul>
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="nosotros.html">Quienes somos</a></li>
+                                <li><a href="galeria.html">Trabajos</a></li>
+                                <li><a href="contacto.html">Contactanos</a></li>
+                            </ul>
+                        </section>
+                    </div>
+                    <div class="footer-bottom">
+                        <div class="social-icons">
+                            <a href="https://www.facebook.com/federico.manzoni.75?locale=es_LA" target="_blank"><i
+                                class="fa fa-facebook"></i></a>
+                            <a href="https://www.instagram.com/padel.fixit/" target="_blank"><i class="fa fa-instagram"></i></a>
+                        </div>
+                        <p>Copyright © 2024 Padel.Fixit | Designed by Emmanuel Rodriguez</p>
+                    </div>
+                </footer>
         </footer>
     );
 }
--- front/src/pages/HomePage.js
+++ front/src/pages/HomePage.js
@@ -20,32 +20,38 @@ const HomePage = (props) => {
     }, []);
 
     return (
-        <section className="hold">
+        <section className="">
+            <div className='hold'>
 
-            <div className="bienvenidos">
-                <h2>Bienvenidos</h2>
-                <p>
-                    En Padel.Fixit, somos especialistas en la reparación y mantenimiento de equipamiento deportivo como raquetas de
-                    tenis, palos de hockey y, especialmente, en el arreglo de paletas de pádel, nuestro servicio más destacado. Además,
-                    ofrecemos tela de carbono de primera calidad, un material fundamental en la fabricación de productos de alto rendimiento
-                    en deportes como el pádel. Desde ajustes precisos en raquetas hasta soluciones integrales para deportistas exigentes,
-                    en Fixit nos comprometemos a proporcionar los mejores servicios y productos para optimizar el rendimiento deportivo.
-                </p>
+                <div className="bienvenidos">
+                    <h2>Bienvenidos</h2>
+                    <p>
+                        En Padel.Fixit, somos especialistas en la reparación y mantenimiento de equipamiento deportivo como raquetas de
+                        tenis, palos de hockey y, especialmente, en el arreglo de paletas de pádel, nuestro servicio más destacado. Además,
+                        ofrecemos tela de carbono de primera calidad, un material fundamental en la fabricación de productos de alto rendimiento
+                        en deportes como el pádel. Desde ajustes precisos en raquetas hasta soluciones integrales para deportistas exigentes,
+                        en Fixit nos comprometemos a proporcionar los mejores servicios y productos para optimizar el rendimiento deportivo.
+                    </p>
+                </div>
+                <h2>Novedades</h2>
+                <div className='novedades'>
+
+                    {
+                        loading ? (
+                            <p>Cargando...</p>
+                        ) : (
+                            novedades.map(item => <NovedadItem key={item.id}
+                                title={item.titulo} subtitle={item.subtitulo}
+                                imagen={item.imagen} body={item.cuerpo} />)
+                        )
+                    }
+                </div >
             </div>
-            <h2>Novedades</h2>
-            <div className='novedades'>
 
-                {
-                    loading ? (
-                        <p>Cargando...</p>
-                    ) : (
-                        novedades.map(item => <NovedadItem key={item.id}
-                            title={item.titulo} subtitle={item.subtitulo}
-                            imagen={item.imagen} body={item.cuerpo} />)
-                    )
-                }
-            </div >
+
         </section>
+
+
     );
 }
 
